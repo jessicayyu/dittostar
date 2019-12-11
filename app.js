22fa367dcb3740785e89d2449891c4b543414426
jessicayyu <jessyu2010@gmail.com>
2019-12-11 00:12:41 -0800

    Using Snoostorm and CommentStream to load comments

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 require('dotenv').config();
 
-const Snoowrap = require('snoowrap');
-const Snoostorm = require('snoostorm');
+const snoowrap = require('snoowrap');
+const { SubmissionStream, CommentStream } = require('snoostorm');
 
-const r = new Snoowrap({
-    userAgent: 'dittostar',
+const r = new snoowrap({
+    userAgent: 'MoriConnect',
     clientId: process.env.CLIENT_ID,
-    clientSecret : process.env.CLIENT_SECRET,
+    clientSecret: process.env.CLIENT_SECRET,
     username: process.env.REDDIT_USER,
     password: process.env.REDDIT_PASS
 });
 
-const client = new Snoostorm(r);
+// const submissions = new SubmissionStream(client, {
+//     subreddit: 'pokemongiveaway',
+//     limit: 10,
+//     pollTime: 5000
+// });
+// submissions.on("submission", console.log);
 
+const comments = new CommentStream(client, {
+    subreddit: 'testingground4bots',
+    limit: 10,
+    pollTime: 5000
+});
+comments.on("comment", (item) => {
+    console.log(item)
+});
