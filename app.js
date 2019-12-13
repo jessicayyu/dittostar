46e1154f08285abe44f9a45f7730b22ef87ae8d8
jessicayyu <jessyu2010@gmail.com>
2019-12-12 23:08:02 -0800

    Connected to Discord

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 
 const snoowrap = require('snoowrap');
-const { SubmissionStream, CommentStream } = require('snoostorm');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const TOKEN = process.env.DISCORD_TOKEN;
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`)
+});
+
+bot.login(TOKEN);
 
 const r = new snoowrap({
     userAgent: 'MoriConnect',
@@ -12,13 +21,14 @@ const r = new snoowrap({
 });
 
 r.getSubreddit('pokemongiveaway')
-    // .getHot().map(post => post.title)
-    .getNewModmailConversations({limit:3})
-    .map(modmail => {
-        let summary = [];
-        summary.push(modmail.id, modmail.subject, modmail.messages[0].author.name.name);
-        return summary;
-    })
-    .map(item => {
-        console.log("Subject: " + item[1] + "\nAuthor:" + item[2] + "\nhttps://mod.reddit.com/mail/all/" + item[0] + "\n")
-    });
+// .getHot().map(post => post.title)
+.getNewModmailConversations({limit:3})
+.map(modmail => {
+    let summary = [];
+    summary.push(modmail.id, modmail.subject, modmail.messages[0].author.name.name);
+    return summary;
+})
+.map(item => {
+    console.log("Subject: " + item[1] + "\nAuthor:" + item[2] + "\nhttps://mod.reddit.com/mail/all/" + item[0] + "\n")
+});
+
