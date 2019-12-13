7210f6ff33337e4ff1d8ce0e57264391904b104c
Jessica Yu <jessyu2010@gmail.com>
2019-12-13 00:41:36 -0800

    Added greets table

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ require('dotenv').config();
 
 const snoowrap = require('snoowrap');
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const disc = new Discord.Client();
 
 const TOKEN = process.env.DISCORD_TOKEN;
 
-bot.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`)
+disc.on('ready', () => {
+    console.log(`Logged in as ${disc.user.tag}!`)
 });
-
-bot.login(TOKEN);
 
 const r = new snoowrap({
     userAgent: 'MoriConnect',
@@ -20,15 +18,42 @@ const r = new snoowrap({
     password: process.env.REDDIT_PASS
 });
 
+function rand(max, min = 0) {
+    return min + Math.floor(Math.random() * Math.floor(max));
+}
+
 r.getSubreddit('pokemongiveaway')
-// .getHot().map(post => post.title)
-.getNewModmailConversations({limit:3})
-.map(modmail => {
-    let summary = [];
-    summary.push(modmail.id, modmail.subject, modmail.messages[0].author.name.name);
-    return summary;
-})
-.map(item => {
-    console.log("Subject: " + item[1] + "\nAuthor:" + item[2] + "\nhttps://mod.reddit.com/mail/all/" + item[0] + "\n")
+    .getNewModmailConversations({limit:3})
+    .map(modmail => {
+        let summary = [];
+        summary.push(modmail.id, modmail.subject, modmail.messages[0].author.name.name);
+        return summary;
+    })
+    .map(item => {
+        const msg = "Subject: " + item[1] + "\nAuthor:" + item[2] + "\nhttps://mod.reddit.com/mail/all/" + item[0] + "\n";
+        console.log(msg);
+    });
+
+disc.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find(ch => ch.name === 'chat-main');
+    const greets = [
+        `Hello, ${member}! So glad to have you here!`, 
+        `Get back in the bag Neb--oh, hi ${member}!`, 
+        `Let's have a champion time, ${member}! Ah ha ha, Leon is so corny, isn't he?`,
+        `Joining us from Galar, ${member}?`,
+        `It's dangerous to go alone, take this! \*hands ${member} some expired coupons\*`
+    ]
+    if (!channel) return;
+    let greeting = greets[rand(4)];
+    channel.send(greeting);
+    console.log('New user joined server!' + member);
 });
 
+disc.on('message', message => {
+    if (message.type === "GUILD_MEMBER_JOIN") {
+        message.delete()
+            .then(console.log('Deleted server greet'))
+    }
+});
+
+disc.login(TOKEN);
