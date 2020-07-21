36c29bf214a677b6285e368bff8481d2e5bfa016
Jessica Yu <jessyu2010@gmail.com>
2020-07-20 23:56:15 -0700

    Mocked out roles tests

--- test/watchers.test.js
+++ test/watchers.test.js
@@ -26,22 +26,55 @@ describe('checkKeywords', function () {
   });
 });
 
-describe('Time Tests', function () {
-  describe('timezoneCheck', function () {
-    function setResult(text) {
-      expect(text).to.include('My phone says');
-    }
-    it('should return a time', function() {
-      watch.timezoneCheck('Europe/Amsterdam', setResult);
+describe('Role testing', function () {
+  const addMock = function() {
+    return new Promise(function(resolve, reject) {
+      if (resolve) return true;
+      if (reject) return false;
     });
+  };
+  const user = {};
+  user.roles = { add: addMock, remove: addMock };
+  user.roles.cache = [{
+    name: 'tester'
+  }]; 
+  const guild = {};
+  guild.roles = { add: addMock, remove: addMock };
+  guild.roles.cache = [{
+    name: 'tester'
+  }, {
+    name: 'dinosaur'
+  }];
+  it('should not toggle remove role b/c they already have it', function() {
+    let result = watch.toggleRole('tester', guild, user);
+    expect(result).to.equal('removed @tester');
+  });
+  it('should add role dinosaur', function() {
+    let result = watch.toggleRole('dinosaur', guild, user);
+    expect(result).to.equal('added @dinosaur');
+  });
+  it('should not add role b/c they already have it, action is add', function() {
+    let result = watch.toggleRole('tester', guild, user, 'add');
+    expect(result).to.equal(false);
+  });
+  it('should not remove role b/c they don\'t have it, action is remove', function() {
+    let result = watch.toggleRole('pancake', guild, user, 'remove');
+    expect(result).to.equal(false);
+  });
+});
+
+describe('Time Tests', function () {
+  function setResult(text) {
+    expect(text).to.include('My phone says');
+  }
+  it('should return a time', function() {
+    watch.timezoneCheck('Europe/Amsterdam', setResult);
   });
 
-  describe('timezoneCheck no location', function() {
-    it('should fail because no location was passed', function() {
-      function testResult(text) {
-        expect(text).to.include('Sorry, I only know the time in');
-      }
-      watch.timezoneCheck(null, testResult);  
-    });
+  it('should fail because no location was passed', function() {
+    function testResult(text) {
+      expect(text).to.include('Sorry, I only know the time in');
+    }
+    watch.timezoneCheck(null, testResult);  
   });
 });
