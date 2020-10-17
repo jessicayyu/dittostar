dc18d6c29afeb6e242d7e548aa265f05761b2ed1
jessicayyu <jessyu2010@gmail.com>
2020-10-16 23:58:02 -0700

    Help dialogue & emoticon updates

--- app.js
+++ app.js
@@ -597,18 +597,6 @@ client.on('message', message => {
       }
     } 
     message.channel.send("Sorry, I don't understand.");
-  } else if (cmd === 'lenny') {
-    message.channel.send('( ͡° ͜ʖ ͡°)');
-  } else if (cmd === 'stare') {
-    message.channel.send('ಠ\\_\\_\\_ಠ');
-  } else if (cmd === 'shrug') {
-    message.channel.send('¯\\_(ツ)_/¯');
-  } else if (cmd === 'denko') {
-    message.channel.send('(´・ω・`)');
-  } else if (cmd === 'tableflip') {
-    message.channel.send('(╯°□°）╯︵ ┻━┻');
-  } else if (cmd === 'magic') {
-    message.channel.send('(ﾉ◕ヮ◕)ﾉ:･ﾟ✧・ﾟ:・ﾟ  : :･ﾟ・ﾟ･✧:・ﾟ  ::･ﾟ:・ﾟ:・ﾟ  ･ﾟ✧:');
   } else if (cmd === 'events') {
     message.channel.send('https://www.reddit.com/r/pokemontrades/wiki/events');
   } else if (cmd === 'ballsprites') {
@@ -623,8 +611,16 @@ client.on('message', message => {
     const embed = new Discord.MessageEmbed()
       .setImage('https://i.imgur.com/qTF3UOi.jpg')
     message.channel.send(`Can we not?? Fine, the picture is over on the wall over there... I'm employee of the month but the other employee *never* shows up. We're gonna get new uniforms soon.`,embed);
+  } else if (cmd === 'tableflip') {
+    message.channel.send('(╯°□°）╯︵ ┻━┻');
   } else if (cmd === 'concern') {
     message.channel.send('<:concern:691821511845085244>');
+  } else if (cmd === 'moriban') {
+    message.channel.send('<moriban:766916174230585394>');
+  } else if (cmd === 'sweat') {
+    message.channel.send('<moriweat:766917539954688031>');
+  } else if (cmd === mori.commandDex.emoticon[cmd]) {
+    message.channel.send(mori.commandDex.emoticon[cmd]);
   }
 });
 
